f72658bc357a6b5ff54a3de55e485f8d549f5802
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-09-18 09:29:00 +0200

    Initial commit

---
+++ src/main/java/fr/eni/Projet_Encheres/ProjetEncheresApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.Projet_Encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEncheresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEncheresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/Projet_Encheres/ProjetEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.Projet_Encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
